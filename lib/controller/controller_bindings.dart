119f6aa547059608c011fd255f32fba461d4f338
HussamBinNoor <hussambinnoor@gmail.com>
2025-05-09 10:38:59 +0500

    added bindings

--- lib/controller/controller_bindings.dart
+++ lib/controller/controller_bindings.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 
 import 'auth_controller.dart';
 
-class ControllerBinding extends Bindings{
+class ControllerBindings extends Bindings{
   @override
   void dependencies(){
     Get.lazyPut(()=>AuthController());
