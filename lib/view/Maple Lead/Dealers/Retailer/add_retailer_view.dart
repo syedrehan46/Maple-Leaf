791217f9369045e4ff758b79a64ac563d7839ac8
HussamBinNoor <hussambinnoor@gmail.com>
2025-06-17 10:44:00 +0500

    Lead converted and processign apis is added

---
+++ lib/view/Maple Lead/Dealers/Retailer/add_retailer_view.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mapleleaf/utils/custom%20widgets/custom_dropdownfeild.dart';
+import 'package:mapleleaf/view/Maple%20Lead/Dealers/Retailer/retailer_custom_design.dart';
+import '../Custom Widgets/custom_apbar_view.dart';
+import '../Dealer Induction/Add Dealer/custom_datepicker_button.dart';
+import '../Job Detail/custom_dropdown_view.dart';
+import '../Job Detail/custom_textfiledd.dart';
+
+class AddRetailerView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+return Scaffold(
+    appBar: CustomAppBar(title: "ADD RETAILER"),
+  body:SafeArea(child:  SingleChildScrollView(
+    child: Column(
+      children: [
+        RetailerCustomDesign(isShowZone: true,isShowArea: true,isShowButton: true,)],
+    ),
+  ),)
+);
+
+  }
+}
